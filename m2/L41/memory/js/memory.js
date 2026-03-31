1235acdd809246d9941290c83eb2dbc1c4d5a52c
captain-mike <info@grafikweb.it>
2026-03-31 21:01:48 +0200

    creazione elementi dom + inizio memory

---
+++ m2/L41/memory/js/memory.js
@@ -0,0 +1,13 @@
+const icone = ['🦊', '🐶', '🐱', '🐸', '🐵', '🐼', '🐷', '🦁'];
+//duplicare le icone
+icone.push(...icone);
+
+//mescolare le icone
+function shuffle(){
+    
+}
+
+// creare una card per ogni icona
+
+
+//creare la funzione startGame()
